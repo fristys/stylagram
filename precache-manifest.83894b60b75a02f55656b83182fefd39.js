9cfe8ec9533fb76247c21152d267cb6794497d03
Momchil Georgiev <fristys@gmail.com>
2019-08-16 19:03:12 +0200

    Updates

---
+++ precache-manifest.83894b60b75a02f55656b83182fefd39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2581844d37bdf2ab6ff773209a9f2a",
+    "url": "/stylagram/index.html"
+  },
+  {
+    "revision": "2ab1206e447873e6a7fa",
+    "url": "/stylagram/static/css/main.aa9cd174.chunk.css"
+  },
+  {
+    "revision": "fc7efeb73814418e37b7",
+    "url": "/stylagram/static/js/2.da9aa554.chunk.js"
+  },
+  {
+    "revision": "2ab1206e447873e6a7fa",
+    "url": "/stylagram/static/js/main.da331029.chunk.js"
+  },
+  {
+    "revision": "a615bdd63106d000f1a5",
+    "url": "/stylagram/static/js/runtime~main.0f981bea.js"
+  }
+]);
